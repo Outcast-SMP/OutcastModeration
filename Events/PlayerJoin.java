34cff6af545c86cb531a2f7022b6693df58b8729
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2022-02-24 08:37:22 +0000

    Update PlayerJoin.java

--- Events/PlayerJoin.java
+++ Events/PlayerJoin.java
@@ -1,14 +1,3 @@
-package me.illusion.outcastmoderation.Events;
-
-import me.illusion.outcastmoderation.Moderation.Utilities;
-import me.illusion.outcastmoderation.OutcastModeration;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-
-import java.util.UUID;
-
 public class PlayerJoin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
