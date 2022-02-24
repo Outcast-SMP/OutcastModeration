76170ecafa3ea7cc0ce31877d69f65d992781d65
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2022-02-24 08:38:04 +0000

    Update PlayerLogin.java

--- Events/PlayerLogin.java
+++ Events/PlayerLogin.java
@@ -1,14 +1,3 @@
-package me.illusion.outcastmoderation.Events;
-
-import me.illusion.outcastmoderation.Moderation.Punishments.Punish;
-import me.illusion.outcastmoderation.Util.Communication.LogMe;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
-
-import java.util.UUID;
-
 public class PlayerLogin implements Listener {
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent e) {
