aef8882dbf77d950474b397c6dbe33eee290451e
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2022-02-24 08:31:23 +0000

    Add files via upload

---
+++ Commands/Vanish.java
@@ -0,0 +1,66 @@
+package me.illusion.outcastmoderation.Commands;
+
+import me.illusion.outcastmoderation.Moderation.Utilities;
+import me.illusion.outcastmoderation.Util.Communication.Chat;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Vanish implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return true;
+        }
+
+        Player ply = (Player) sender;
+        UUID pID = ply.getUniqueId();
+
+        if (args.length <= 0) {
+            if (!new Utilities(ply).vanish()) {
+                new Chat(ply, "&cUnable to vanish / unvanish yourself.").message(true);
+            }
+            new Chat(ply, "&7You are now vanished from other players...").message(true);
+            return true;
+        }
+
+        String playerName = args[0];
+
+        if (!new Utilities(playerName).vanish()) {
+            new Chat(ply, "&cUnable to vanish / unvanish '&c&l" + playerName + "&7'.").message(true);
+        }
+
+        new Chat(ply, "&7You are now vanished from other players...").message(true);
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        String commandName = command.getName();
+
+        if (commandName.equals("vanish") || commandName.equals("v")) {
+            List<String> itemList = new ArrayList<>();
+
+            switch (args.length) {
+                case 1:
+                    for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                        itemList.add(onlinePlayer.getName());
+                    }
+                    itemList.add("[player name]");
+                    break;
+                default:
+                    itemList.add("Invalid amount of arguments.");
+                    break;
+            }
+            return itemList;
+        }
+        return null;
+    }
+}
