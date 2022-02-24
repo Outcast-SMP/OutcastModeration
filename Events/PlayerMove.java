04b455aebfbe29fac711f9a29e1c60ac4bad4015
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2022-02-24 08:38:25 +0000

    Update PlayerMove.java

--- Events/PlayerMove.java
+++ Events/PlayerMove.java
@@ -1,13 +1,3 @@
-package me.illusion.outcastmoderation.Events;
-
-import me.illusion.outcastmoderation.Moderation.Punishments.Punish;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
-
-import java.util.UUID;
-
 public class PlayerMove implements Listener {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
