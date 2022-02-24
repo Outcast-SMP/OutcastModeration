74d400a7a79e9d7b8192ee10d624d8405e6eed56
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2022-02-24 08:37:45 +0000

    Update PlayerLeave.java

--- Events/PlayerLeave.java
+++ Events/PlayerLeave.java
@@ -1,13 +1,3 @@
-package me.illusion.outcastmoderation.Events;
-
-import me.illusion.outcastmoderation.Moderation.Utilities;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerQuitEvent;
-
-import java.util.UUID;
-
 public class PlayerLeave implements Listener {
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent e) {
