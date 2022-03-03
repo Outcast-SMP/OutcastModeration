2032eff6df9483571c7e7bfde4106cba07abd25f
IllusionzDev <68855711+IllusionzDev@users.noreply.github.com>
2022-03-03 19:10:42 +0000

    Delete Events directory

--- Events/PlayerLeave.java
+++
@@ -1,9 +0,0 @@
-public class PlayerLeave implements Listener {
-    @EventHandler
-    public void onPlayerLeave(PlayerQuitEvent e) {
-        Player ply = e.getPlayer();
-        UUID pID = ply.getUniqueId();
-
-        Utilities.getVanishedPlayerList().remove(ply);
-    }
-}
--- Events/PlayerLogin.java
+++
@@ -1,9 +0,0 @@
-public class PlayerLogin implements Listener {
-    @EventHandler
-    public void onPlayerLogin(PlayerLoginEvent e) {
-        Player ply = e.getPlayer();
-        UUID pID = ply.getUniqueId();
-
-        Punish.banCheck(e, ply);
-    }
-}
